a2d793f7eb410988b9fc304d9f521fe5bd0a80bc
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-09-29 12:03:02 +0200

    add negative digit

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 import time
 import pygame
+from decimal import Decimal
 
 
 def say_digit_or_operation(value):
@@ -9,51 +10,60 @@ def say_digit_or_operation(value):
 
 def say_answer(ans):
     say_digit_or_operation('equal')
-    for num in str(ans):
+    if ans == 'error_zero_division':
         time.sleep(0.8)
-        if num.isdigit():
-            say_digit_or_operation(num)
-        elif num == '-':
-            say_digit_or_operation('negative')
-        elif num == '.':
-            say_digit_or_operation('point')
+        say_digit_or_operation(ans)
+    else:
+        for num in str(ans):
+            time.sleep(0.8)
+            if num.isdigit():
+                say_digit_or_operation(num)
+            elif num == '-':
+                say_digit_or_operation('negative')
+            elif num == '.':
+                say_digit_or_operation('point')
+
+
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def is_float(value: str):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
 
 def calculate(value1: str, value2: str, op: str) -> (int, float):
+    values = [value1, value2]
+    for i in range(len(values)):
+        if is_int(values[i]):
+            values[i] = int(values[i])
+        elif is_float(values[i]):
+            values[i] = float(values[i])
+        else:
+            return 'invalid values'
+
     match op:
         case '+':
-            return int(value2) + int(value1)
+            return values[1] + values[0]
         case '-':
-            return int(value2) - int(value1)
+            return values[1] - values[0]
         case '*':
-            return int(value2) * int(value1)
+            return values[1] * values[0]
         case '/':
-            return int(value2) / int(value1)
-
-
-def digit_key_decoder(key):
-    match key:
-        case pygame.K_KP0:
-            return '0'
-        case pygame.K_KP1:
-            say_digit_or_operation('1')
-            return '1'
-        case pygame.K_KP2:
-            return '2'
-        case pygame.K_KP3:
-            return '3'
-        case pygame.K_KP4:
-            return '4'
-        case pygame.K_KP5:
-            return '5'
-        case pygame.K_KP6:
-            return '6'
-        case pygame.K_KP7:
-            return '7'
-        case pygame.K_KP8:
-            return '8'
-        case pygame.K_KP9:
-            return '9'
+            try:
+                num = Decimal(values[1] / values[0])
+                print(num)
+                return num.quantize(Decimal('1.00'))
+            except ZeroDivisionError:
+                return 'error_zero_division'
 
 
 def main():
@@ -74,12 +84,25 @@ def main():
 
     num1 = ''
     buf = ''
-    num_operators = {
+
+    ariphemitic_operators = {
         pygame.K_KP_PLUS: ('plus', '+'),
         pygame.K_KP_MINUS: ('minus', '-'),
         pygame.K_KP_DIVIDE: ('divide', '/'),
         pygame.K_KP_MULTIPLY: ('multiply', '*'),
-        pygame.K_EQUALS: ('equal', '=')
+    }
+
+    num_pad_digits = {
+        pygame.K_KP0: '0',
+        pygame.K_KP1: '1',
+        pygame.K_KP2: '2',
+        pygame.K_KP3: '3',
+        pygame.K_KP4: '4',
+        pygame.K_KP5: '5',
+        pygame.K_KP6: '6',
+        pygame.K_KP7: '7',
+        pygame.K_KP8: '8',
+        pygame.K_KP9: '9'
     }
 
     while 1:
@@ -88,27 +111,29 @@ def main():
                 case pygame.QUIT:
                     exit()
                 case pygame.KEYDOWN:
-                    if event.key not in num_operators.keys():
-                        input_value = digit_key_decoder(event.key)
-                        if input_value:
-                            num1 += input_value
-                            print(input_value, end='')
-                            say_digit_or_operation(input_value)
+                    if event.key in num_pad_digits.keys():
+                        num1 += num_pad_digits[event.key]
+                        print(num_pad_digits[event.key], end='')
+                        say_digit_or_operation(num_pad_digits[event.key])
 
-                    elif event.key in num_operators.keys() and event.key != pygame.K_EQUALS:
+                    elif event.key in ariphemitic_operators.keys():
                         buf = num1
                         num1 = ''
-                        operator = num_operators[event.key]
+                        operator = ariphemitic_operators[event.key]
                         print(operator[1], end='')
                         say_digit_or_operation(operator[0])
 
-                    elif event.key == pygame.K_EQUALS:
+                    elif event.key in (pygame.K_PERIOD, pygame.K_KP_PERIOD):
+                        num1 += '.'
+                        print('.', end='')
+                        say_digit_or_operation('point')
+
+                    elif event.key == pygame.K_KP_ENTER:
                         answer = calculate(num1, buf, operator[1])
                         print(f' = {answer}')
                         say_answer(answer)
                         num1 = ''
                         buf = ''
-
         clock.tick(FPS)
 
 

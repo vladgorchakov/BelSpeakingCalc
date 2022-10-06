942fad2bd22bf9ac14da956df2216ba4f02b0982
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-10-06 16:27:49 +0200

    add new win

--- Calc.py
+++ Calc.py
@@ -20,7 +20,7 @@ class Window:
 
 
 class OperandWindow(Window):
-    def __init__(self, sc, weight, height, color, position, font_size=72):
+    def __init__(self, sc, weight, height, color, position, font_size=128):
         super().__init__(weight, height, color)
         self.surface = pygame.Surface((self.w, self.h))
         self.surface.fill(self.window_color)
@@ -28,20 +28,15 @@ class OperandWindow(Window):
         self.sc = sc
         self.position = position
 
-
-    def clear(self):
-        self.surface.fill(self.window_color)
-        pygame.display.update()
-
-    def write(self, text, font_color=Colors.BLACK.value):
+    def write(self, text, font_color=Colors.WHITE.value):
         font_surface = self.font.render(text, 1, font_color, self.window_color)
         font_pos = font_surface.get_rect(center=(self.w // 2, self.h // 2))
         self.surface.blit(font_surface, font_pos)
         self.sc.blit(self.surface, self.surface.get_rect(topleft=self.position))
         pygame.display.update()
 
-    def get(self):
-        return self
+    def clear(self):
+        self.surface.fill(self.window_color)
 
 
 class Calculator(Window):
@@ -51,12 +46,33 @@ class Calculator(Window):
         self.caption = caption
         self.window = pygame.display.set_mode((self.w, self.h))
         # self.op = [OperandWindow(self.window, self.w, 200, Colors.BLACK.value, (0, i)) for i in range(0, self.h, self.h // 4)]
-        self.op1 = OperandWindow(self.window, 1000, 200, Colors.GREEN.value, (0, 0))
-        self.op2 = OperandWindow(self.window, 1000, 200, Colors.GREEN.value, (0, 300))
-        self.operation = OperandWindow(self.window, 1000, 200, Colors.BLUE.value, (0, 150))
-        self.answer = OperandWindow(self.window, 1000, 200, Colors.WHITE.value, (0, 450))
+        self.op1 = OperandWindow(self.window, 1000, 150, Colors.BLACK.value, (0, 0))
+        self.op2 = OperandWindow(self.window, 1000, 150, Colors.BLACK.value, (0, 300))
+        self.operation = OperandWindow(self.window, 1000, 150, Colors.BLACK.value, (0, 150))
+        self.answer = OperandWindow(self.window, 1000, 150, Colors.BLACK.value, (0, 450))
         self.fps = 60
         self.clock = pygame.time.Clock()
+
+        self.ariphemitic_operators = {
+            pygame.K_KP_PLUS: ('plus', '+'),
+            pygame.K_KP_MINUS: ('minus', '-'),
+            pygame.K_KP_DIVIDE: ('divide', '/'),
+            pygame.K_KP_MULTIPLY: ('multiply', '*'),
+        }
+
+        self.num_pad_digits = {
+            pygame.K_KP0: '0',
+            pygame.K_KP1: '1',
+            pygame.K_KP2: '2',
+            pygame.K_KP3: '3',
+            pygame.K_KP4: '4',
+            pygame.K_KP5: '5',
+            pygame.K_KP6: '6',
+            pygame.K_KP7: '7',
+            pygame.K_KP8: '8',
+            pygame.K_KP9: '9'
+        }
+
 
     @staticmethod
     def say_digit_or_operation(value):
@@ -123,49 +139,40 @@ class Calculator(Window):
         self.window.fill(self.window_color)
         pygame.display.update()
 
+    def clear_fields(self):
+        self.op1.clear()
+        self.op2.clear()
+        self.operation.clear()
+        self.answer.clear()
+
     def run(self):
         self.show_window()
 
         num1 = ''
         buf = ''
-
-        ariphemitic_operators = {
-            pygame.K_KP_PLUS: ('plus', '+'),
-            pygame.K_KP_MINUS: ('minus', '-'),
-            pygame.K_KP_DIVIDE: ('divide', '/'),
-            pygame.K_KP_MULTIPLY: ('multiply', '*'),
-        }
-
-        num_pad_digits = {
-            pygame.K_KP0: '0',
-            pygame.K_KP1: '1',
-            pygame.K_KP2: '2',
-            pygame.K_KP3: '3',
-            pygame.K_KP4: '4',
-            pygame.K_KP5: '5',
-            pygame.K_KP6: '6',
-            pygame.K_KP7: '7',
-            pygame.K_KP8: '8',
-            pygame.K_KP9: '9'
-        }
-
         op = self.op1
+
+        clear = False
         while True:
             for event in pygame.event.get():
                 match event.type:
                     case pygame.QUIT:
                         exit()
                     case pygame.KEYDOWN:
-                        if event.key in num_pad_digits.keys():
-                            num1 += num_pad_digits[event.key]
-                            print(num_pad_digits[event.key], end='')
-                            op.write(num1)
-                            self.say_digit_or_operation(num_pad_digits[event.key])
+                        if event.key in self.num_pad_digits.keys():
+                            if clear:
+                                self.window.fill(self.window_color)
+                                clear = False
 
-                        elif event.key in ariphemitic_operators.keys():
+                            num1 += self.num_pad_digits[event.key]
+                            print(self.num_pad_digits[event.key], end='')
+                            op.write(num1)
+                            self.say_digit_or_operation(self.num_pad_digits[event.key])
+
+                        elif event.key in self.ariphemitic_operators.keys():
                             buf = num1
                             num1 = ''
-                            operator = ariphemitic_operators[event.key]
+                            operator = self.ariphemitic_operators[event.key]
                             print(operator[1], end='')
                             self.operation.write(operator[1])
                             self.say_digit_or_operation(operator[0])
@@ -180,15 +187,15 @@ class Calculator(Window):
                         elif event.key == pygame.K_KP_ENTER:
                             answer = self.calculate(num1, buf, operator[1])
                             print(f' = {answer}')
-                            self.answer.write(str(answer))
+                            self.answer.write(f'={answer}')
                             self.say_answer(answer)
                             num1 = ''
                             buf = ''
                             op = self.op1
-
+                            clear = True
 
             self.clock.tick(self.fps)
 
+
 calc = Calculator()
 calc.run()
-
